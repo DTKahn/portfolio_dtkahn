3a777837fd84b15e61ef961e615591560b554b3a
Daniel <dtkahn@gmail.com>
2018-09-18 12:35:59 -0400

    mobile nav and hamburger added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,22 +1,22 @@
-import Granim from 'granim';
+// import Granim from 'granim';
 
-const granimInstance = new Granim({
-    element: '#torontoBg',
-    direction: 'top-bottom',
-    opacity: [1, 1],
-    isPausedWhenNotInView: true,
-    states: {
-        "default-state": {
-            gradients: [
-                ['#29323c', '#485563'],
-                ['#FF6B6B', '#556270'],
-                ['#80d3fe', '#7ea0c4'],
-                ['#f0ab51', '#eceba3']
-            ],
-            transitionSpeed: 7000
-        }
-    }
-});
+// const granimInstance = new Granim({
+//     element: '#torontoBg',
+//     direction: 'top-bottom',
+//     opacity: [1, 1],
+//     isPausedWhenNotInView: true,
+//     states: {
+//         "default-state": {
+//             gradients: [
+//                 ['#29323c', '#485563'],
+//                 ['#FF6B6B', '#556270'],
+//                 ['#80d3fe', '#7ea0c4'],
+//                 ['#f0ab51', '#eceba3']
+//             ],
+//             transitionSpeed: 7000
+//         }
+//     }
+// });
 
 $(function () {
     $('a').smoothScroll();
